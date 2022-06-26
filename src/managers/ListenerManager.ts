70263bc27ae3befeecf12943985b3d5a71ed83f1
richaardev <richardmartins.yt@gmail.com>
2022-06-26 02:06:58 -0300

    .

--- src/managers/ListenerManager.ts
+++ src/managers/ListenerManager.ts
@@ -3,7 +3,7 @@ import NanaClient from "@/NanaClient";
 import { BaseListener, BaseManager } from "@/structures";
 import { Logger } from "@/utils";
 import glob from "glob";
-import path from "path"
+import path from "path";
 
 export default class ListenerManager extends BaseManager {
   constructor(client: NanaClient, public options: ListenerManagerOptions) {
@@ -11,26 +11,29 @@ export default class ListenerManager extends BaseManager {
   }
 
   override load(): boolean {
+    const uncapitalize = (str: string) => str.charAt(0).toLowerCase() + str.slice(1);
+    type ListenerClassType = { default: new (client: NanaClient) => BaseListener };
+
     glob("src/listeners/**/*.{js,ts}", (err, files) => {
       if (err) throw err;
       files.forEach(async (file) => {
-        const { default: ListenerClass }: { default: new (client: NanaClient) => BaseListener } = await import(file);
-        const listener = new ListenerClass(this.client);
-        const methods = Object.getOwnPropertyNames(ListenerClass.prototype).filter(
-          (method) => method.startsWith("on"),
-        );
-        const uncapitalize = (str: string) => str.charAt(0).toLowerCase() + str.slice(1);
-        methods.forEach((method) => {
-          const eventName = uncapitalize(method.replace("on", ""));
-          try {
-            // @ts-ignore
-            this.client.on(eventName, (...args) => listener[method](...args));
-            Logger.info("Events initialized");
-            return true;
-          } catch (err) {}
+        import(file).then(({ default: ListenerClass }: ListenerClassType) => {
+          const listener = new ListenerClass(this.client);
+          const methods = Object.getOwnPropertyNames(ListenerClass.prototype).filter((method) =>
+            method.startsWith("on"),
+          );
+          methods.forEach((method) => {
+            const eventName = uncapitalize(method.replace("on", ""));
+            try {
+              // @ts-ignore
+              this.client.on(eventName, (...args) => listener[method](...args));
+              Logger.info("Listeners Initialized");
+              return true;
+            } catch (err) {}
+          });
         });
       });
     });
-    return true;
+    return false;
   }
 }
